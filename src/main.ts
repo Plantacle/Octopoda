7de749c4540eb83d0b45b6d641bbfd95d37bd984
Louis Laureys <louis@laureys.me>
2019-12-30 22:49:41 +0100

    Enable swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
 
 declare const module: any
+const baseurl = '/api'
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix(baseurl)
+
+  const swaggerDocument = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('Plantacle REST API')
+      .setVersion('1.0')
+      .addTag('Measurements')
+      .build(),
+  )
+  SwaggerModule.setup(`${baseurl}/swagger`, app, swaggerDocument, {
+    swaggerOptions: {
+      defaultModelsExpandDepth: 0,
+      operationsSorter: 'method',
+      displayRequestDuration: true,
+    },
+  })
 
   await app.listen(3123)
 
